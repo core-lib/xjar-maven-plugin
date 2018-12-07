87b4e519cfe30d76590f3418b31ff31dbe6fefcc
core-lib <646742615>
2018-12-07 14:43:15 +0800

    1. 将对普通jar项目或模块和spring boot项目或模块的插件方式加密整合成一个智能分析goal，配置时无需关心当前项目或模块是哪一种。

--- src/main/java/io/xjar/XBuilder.java
+++ src/main/java/io/xjar/XBuilder.java
@@ -1,13 +1,25 @@
 package io.xjar;
 
+import io.xjar.boot.XBoot;
+import io.xjar.filter.XAllEntryFilter;
+import io.xjar.filter.XAnyEntryFilter;
+import io.xjar.jar.XJar;
 import org.apache.commons.compress.archivers.jar.JarArchiveEntry;
+import org.apache.maven.model.Build;
+import org.apache.maven.model.Plugin;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
 import org.apache.maven.plugin.logging.Log;
+import org.apache.maven.plugins.annotations.LifecyclePhase;
+import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
+import org.apache.maven.project.MavenProject;
+import org.codehaus.plexus.util.xml.Xpp3Dom;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.Map;
 
 /**
  * XJar 构建插件
@@ -15,55 +27,61 @@ import java.io.IOException;
  * @author Payne 646742615@qq.com
  * 2018/12/4 10:27
  */
-public abstract class XBuilder extends AbstractMojo {
+@Mojo(name = "build", defaultPhase = LifecyclePhase.PACKAGE)
+public class XBuilder extends AbstractMojo {
+    /**
+     * 当前Maven工程
+     */
+    @Parameter(defaultValue = "${project}", readonly = true, required = true)
+    private MavenProject project;
 
     /**
      * 加密算法名称
      */
     @Parameter(property = "xjar.algorithm", required = true, defaultValue = "AES")
-    protected String algorithm;
+    private String algorithm;
 
     /**
      * 加密密钥长度
      */
     @Parameter(property = "xjar.keySize", required = true, defaultValue = "128")
-    protected int keySize;
+    private int keySize;
 
     /**
      * 加密向量长度
      */
     @Parameter(property = "xjar.ivSize", required = true, defaultValue = "128")
-    protected int ivSize;
+    private int ivSize;
 
     /**
      * 加密密码
      */
     @Parameter(property = "xjar.password", required = true)
-    protected String password;
+    private String password;
 
     /**
      * 原本JAR所在文件夹
      */
     @Parameter(property = "xjar.sourceDir", required = true, defaultValue = "${project.build.directory}")
-    protected File sourceDir;
+    private File sourceDir;
 
     /**
      * 原本JAR名称
      */
     @Parameter(property = "xjar.sourceJar", required = true, defaultValue = "${project.build.finalName}.jar")
-    protected String sourceJar;
+    private String sourceJar;
 
     /**
      * 生成JAR所在文件夹
      */
     @Parameter(property = "xjar.targetDir", required = true, defaultValue = "${project.build.directory}")
-    protected File targetDir;
+    private File targetDir;
 
     /**
      * 生成JAR名称
      */
     @Parameter(property = "xjar.targetJar", required = true, defaultValue = "${project.build.finalName}.xjar")
-    protected String targetJar;
+    private String targetJar;
 
     /**
      * 包含资源，避免和excludes配置一起使用，如果混合使用则excludes失效。
@@ -73,7 +91,7 @@ public abstract class XBuilder extends AbstractMojo {
      * BOOT-INF/lib/*.jar
      */
     @Parameter(property = "xjar.includes")
-    protected String[] includes;
+    private String[] includes;
 
     /**
      * 排除资源，避免和includes配置一起使用，如果混合使用则excludes失效。
@@ -83,16 +101,11 @@ public abstract class XBuilder extends AbstractMojo {
      * BOOT-INF/lib/*.jar
      */
     @Parameter(property = "xjar.excludes")
-    protected String[] excludes;
+    private String[] excludes;
 
-    /**
-     * 项目打包模式，只对packaging == jar 的模块构建XJar
-     */
-    @Parameter(readonly = true, defaultValue = "${project.packaging}")
-    protected String packaging;
-
-    public void execute() throws MojoExecutionException {
+    public void execute() throws MojoExecutionException, MojoFailureException {
         Log log = getLog();
+        String packaging = project.getPackaging();
         if (!"jar".equalsIgnoreCase(packaging)) {
             log.info("Skip for packaging: " + packaging);
             return;
@@ -113,14 +126,14 @@ public abstract class XBuilder extends AbstractMojo {
             log.info("Building xjar: " + dest + " for jar: " + src);
             XEntryFilter<JarArchiveEntry> filter;
             if (includes != null && includes.length > 0) {
-                XAnyEntryFilter<JarArchiveEntry> xIncludesFilter = XEntryFilters.any();
+                XAnyEntryFilter<JarArchiveEntry> xIncludesFilter = XKit.any();
                 for (int i = 0; includes != null && i < includes.length; i++) {
                     xIncludesFilter.mix(new XIncludeAntEntryFilter(includes[i]));
                     log.info("Including " + includes[i]);
                 }
                 filter = xIncludesFilter;
             } else if (excludes != null && excludes.length > 0) {
-                XAllEntryFilter<JarArchiveEntry> xExcludesFilter = XEntryFilters.all();
+                XAllEntryFilter<JarArchiveEntry> xExcludesFilter = XKit.all();
                 for (int i = 0; excludes != null && i < excludes.length; i++) {
                     xExcludesFilter.mix(new XExcludeAntEntryFilter(excludes[i]));
                     log.info("Excluding " + excludes[i]);
@@ -130,25 +143,44 @@ public abstract class XBuilder extends AbstractMojo {
                 filter = null;
                 log.info("Including all resources");
             }
-            build(src, dest, password, algorithm, keySize, ivSize, filter);
+            Build build = project.getBuild();
+            Map<String, Plugin> plugins = build.getPluginsAsMap();
+            Plugin plugin = plugins.get("org.springframework.boot:spring-boot-maven-plugin");
+            // 非Spring-Boot项目/模块
+            if (plugin == null) {
+                XJar.encrypt(src, dest, password, algorithm, keySize, ivSize, filter);
+            }
+            // Spring-Boot项目/模块
+            else {
+                Object configuration = plugin.getConfiguration();
+                // 不允许开启 <executable>true<executable>
+                if (configuration instanceof Xpp3Dom) {
+                    Xpp3Dom dom = (Xpp3Dom) configuration;
+                    Xpp3Dom child = dom.getChild("executable");
+                    String executable = child != null ? child.getValue() : null;
+                    if ("true".equalsIgnoreCase(executable)) {
+                        String msg = "Unsupported to build an xjar for an <executable>true</executable> spring boot JAR file, ";
+                        msg += "maybe you should upgrade xjar-maven-plugin dependency if it have been supported in the later versions,";
+                        msg += "if not, delete <executable>true</executable> or set executable as false for the configuration of spring-boot-maven-plugin.";
+                        throw new MojoFailureException(msg);
+                    }
+                }
+                XBoot.encrypt(src, dest, password, algorithm, keySize, ivSize, filter);
+            }
+        } catch (MojoExecutionException | MojoFailureException e) {
+            throw e;
         } catch (Exception e) {
             throw new MojoExecutionException("could not build xjar", e);
         }
     }
 
-    /**
-     * 构建XJar包
-     *
-     * @param src       原文包
-     * @param dest      加密包
-     * @param password  密码
-     * @param algorithm 加密算法
-     * @param keySize   密钥长度
-     * @param ivSize    向量长度
-     * @param filter    过滤器
-     * @throws Exception 加密异常
-     */
-    protected abstract void build(File src, File dest, String password, String algorithm, int keySize, int ivSize, XEntryFilter<JarArchiveEntry> filter) throws Exception;
+    public MavenProject getProject() {
+        return project;
+    }
+
+    public void setProject(MavenProject project) {
+        this.project = project;
+    }
 
     public String getAlgorithm() {
         return algorithm;
@@ -228,13 +260,5 @@ public abstract class XBuilder extends AbstractMojo {
 
     public void setExcludes(String[] excludes) {
         this.excludes = excludes;
-    }
-
-    public String getPackaging() {
-        return packaging;
-    }
-
-    public void setPackaging(String packaging) {
-        this.packaging = packaging;
     }
 }
